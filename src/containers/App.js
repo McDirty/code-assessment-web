c8d1fb7df78ef338e38611b9402cd2755197de64
Adam McCurdy <adam@codetronix.com>
2019-02-28 20:43:19 -0800

    Intial commit, adding header styles, and cart asset

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,13 +1,21 @@
 import React from 'react'
 import ProductsContainer from './ProductsContainer'
 import CartContainer from './CartContainer'
+import styles from './App.css'
+import cartSVG from '../media/svg/cartIcon.svg'
 
 const App = () => (
-  <div>
-    <h2>Shopping Cart Example</h2>
-    <hr/>
+  <div className={styles.container}>
+    <div className={styles.header}>
+      <div className={styles.headLabel}>Acme Store</div>
+      <div className={styles.cart}>
+        <img src={cartSVG} alt="Cart Icon" />
+        <div className={styles.cartLabel}>Your cart is empty</div>
+      </div>
+    </div>
+    <hr />
     <ProductsContainer />
-    <hr/>
+    <hr />
     <CartContainer />
   </div>
 )
